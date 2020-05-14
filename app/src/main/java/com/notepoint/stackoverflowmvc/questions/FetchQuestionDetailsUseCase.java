6d905021b7253c4570cf04e6979180fc61c8dd25
sumandora04 <sumandora04@gmail.com>
2020-05-14 12:08:23 +0530

    FetchQuestionDetailsUseCase: Separate networking logic from activity. BaseObservable: Separate listeners for handling the observer pattern.

---
+++ app/src/main/java/com/notepoint/stackoverflowmvc/questions/FetchQuestionDetailsUseCase.java
@@ -0,0 +1,67 @@
+package com.notepoint.stackoverflowmvc.questions;
+/*
+     Created by Suman on 5/14/2020.
+*/
+
+import android.widget.Toast;
+
+import com.notepoint.stackoverflowmvc.common.BaseObservable;
+import com.notepoint.stackoverflowmvc.networking.QuestionDetailsResponseSchema;
+import com.notepoint.stackoverflowmvc.networking.QuestionSchema;
+import com.notepoint.stackoverflowmvc.networking.StackoverflowApi;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FetchQuestionDetailsUseCase extends BaseObservable<FetchQuestionDetailsUseCase.Listener> {
+
+    private final StackoverflowApi mStackoverflowApi;
+
+    public FetchQuestionDetailsUseCase(StackoverflowApi mStackoverflowApi) {
+        this.mStackoverflowApi = mStackoverflowApi;
+    }
+
+    public interface Listener{
+        void onQuestionDetailFetched(QuestionDetails questionDetails);
+
+        void onQuestionDetailFetchFailed();
+    }
+
+
+    public void fetchQuestionDetailsAndNotify(String questionId){
+        mStackoverflowApi.fetchQuestionDetails(questionId)
+                .enqueue(new Callback<QuestionDetailsResponseSchema>() {
+                    @Override
+                    public void onResponse(Call<QuestionDetailsResponseSchema> call, Response<QuestionDetailsResponseSchema> response) {
+                        if (response.isSuccessful()){
+                            notifySuccess(response.body().getQuestion());
+                        }else {
+                            notifyFailure();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<QuestionDetailsResponseSchema> call, Throwable t) {
+                        notifyFailure();
+                    }
+                });
+    }
+
+
+    private void notifySuccess(QuestionSchema questionSchema) {
+
+        for (Listener listener: getListeners()){
+            listener.onQuestionDetailFetched(new QuestionDetails(
+                    questionSchema.getId(),
+                    questionSchema.getTitle(),
+                    questionSchema.getBody()));
+        }
+    }
+
+    private void notifyFailure() {
+        for (Listener listener: getListeners()){
+            listener.onQuestionDetailFetchFailed();
+        }
+    }
+}
